2466501e8fc7f45d3c0fd091b08333d8a1952cfb
Rhys Ulerich <rhys.ulerich@gmail.com>
2019-03-13 21:54:20 -0400

    Getting towards parsing

--- zc.c
+++ zc.c
@@ -2,8 +2,17 @@
  * zc: C version of https://github.com/rupa/z.
  * Licensed under the Apache License, Version 2.0.
  */
+#define _POSIX_C_SOURCE 2
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/time.h>
+#include <unistd.h>
+
+#define die(...) do {                  \
+        fprintf (stderr, __VA_ARGS__); \
+        fputc('\n', stderr);           \
+        exit(EXIT_FAILURE); }          \
+    while (0)
 
 long milliseconds() {
     struct timeval now;
@@ -13,5 +22,15 @@ long milliseconds() {
 
 int main(int argc, char **argv)
 {
+    int opt;
+    while ((opt = getopt(argc, argv, "h")) != -1) {
+        switch (opt) {
+        case 'h':
+            die("Unimplemented");
+        default:
+            die("Usage: %s", argv[0]);
+        }
+    }
     printf("Hello %ld\n", milliseconds());
+    exit(EXIT_SUCCESS);
 }
