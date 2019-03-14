d810be2c8fd141cd5abb069c558447ce1684bd32
Rhys Ulerich <rhys.ulerich@gmail.com>
2019-03-13 22:30:15 -0400

    Begin database

--- zc.c
+++ zc.c
@@ -2,10 +2,15 @@
  * zc: C version of https://github.com/rupa/z.
  * Licensed under the Apache License, Version 2.0.
  */
-#define _POSIX_C_SOURCE 2
+#define _POSIX_C_SOURCE 200809L
+#include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 #include <sys/time.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 #define die(...) do {                  \
@@ -20,17 +25,25 @@ long milliseconds() {
     return 1000*now.tv_sec + now.tv_usec/1000;
 }
 
+// --add
+// --complete
 int main(int argc, char **argv)
 {
+    int fd = -1;
     int opt;
-    while ((opt = getopt(argc, argv, "h")) != -1) {
+    while ((opt = getopt(argc, argv, "f:h")) != -1) {
         switch (opt) {
+        case 'f':
+            fd = open(optarg, O_CREAT | O_RDWR, 0600);
+            if (fd < 0) die("Failed opening '%s' (%d): %s",
+                            optarg, errno, strerror(errno));
+            break;
         case 'h':
             die("Unimplemented");
         default:
-            die("Usage: %s", argv[0]);
+            die("Usage: %s -F DATABASE", argv[0]);
         }
     }
-    printf("Hello %ld\n", milliseconds());
+    printf("Hello %d %ld\n", fd, milliseconds());
     exit(EXIT_SUCCESS);
 }
