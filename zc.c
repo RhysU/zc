9eabab3d2621f2b7c34ea08acd0522cddd8df5a0
Rhys Ulerich <rhys.ulerich@gmail.com>
2019-03-13 21:31:03 -0400

    Clock

--- zc.c
+++ zc.c
@@ -3,8 +3,15 @@
  * Licensed under the Apache License, Version 2.0.
  */
 #include <stdio.h>
+#include <sys/time.h>
+
+long milliseconds() {
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    return 1000*now.tv_sec + now.tv_usec/1000;
+}
 
 int main(int argc, char **argv)
 {
-    printf("Hello\n");
+    printf("Hello %ld\n", milliseconds());
 }
